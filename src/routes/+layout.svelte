8e7e91142dd502cd9d3e80f6a3e1a79ebeb595e1
Doug A <douganderson444@gmail.com>
2022-11-25 17:00:07 -0400

    sw to tailwindcss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
-<div
-	style="display: grid;width: 300px;height: 300px;border: 1px solid gold;align-items: center;justify-items: center;"
->
+<script>
+	import '../app.css';
+</script>
+
+<div class="grid w-96 h-96 border-2 border-green-500 items-center justify-items-center">
 	<slot />
 </div>
