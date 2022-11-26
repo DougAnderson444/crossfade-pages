197709730d75b579beb6323a0523aa114aeaa467
Doug A <douganderson444@gmail.com>
2022-11-26 16:03:59 -0400

    sketch theme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Crossfade from '$lib/Crossfade.svelte';
+	import Sketched from '$lib/Themes/Sketched.svelte';
 	import { items } from '$lib/stores';
 	import { nanoid } from 'nanoid/non-secure';
 	import Expand from '$lib/SVGs/Expand.svelte';
@@ -20,16 +21,16 @@
 	{#if $items && $items.length}
 		{#each $items as item, i}
 			<Crossfade id={item.id}>
-				<div
-					class="flex flex-col flex-wrap w-24 h-24 m-2 bg-white border border-gray-200 rounded-lg shadow"
-				>
-					<a href="/foo" class="select-none cursor-pointer" contenteditable={false}
-						><div class="float-right top-0 right-0 h-6 w-6 m-1 text-neutral-800/50">
-							<Expand />
-						</div></a
-					>
-					{item.id}
-				</div>
+				<Sketched>
+					<div class="flex flex-col flex-wrap w-full h-full">
+						<a href="/foo" class="select-none cursor-pointer" contenteditable={false}
+							><div class="float-right top-0 right-0 h-6 w-6 m-1 text-neutral-800/50">
+								<Expand />
+							</div></a
+						>
+						{item.id}
+					</div>
+				</Sketched>
 			</Crossfade>
 		{/each}
 	{/if}
