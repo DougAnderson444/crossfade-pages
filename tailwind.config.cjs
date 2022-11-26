0ad326250edec38309fbff5eb9ca7db81dc2aedd
Doug A <douganderson444@gmail.com>
2022-11-26 14:03:50 -0400

    add font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		// for production: https://blog.logrocket.com/how-to-use-custom-fonts-tailwind-css/
+		// fontFamily: {
+		// 	architect: ['Architects Daughter', 'cursive'],
+		// 	bangers: ['Bangers', 'cursive'],
+		// 	hand: ['Just Another Hand', 'cursive']
+		// },
+		extend: {
+			fontFamily: {
+				architect: ['Architects Daughter', 'cursive'],
+				bangers: ['Bangers', 'cursive'],
+				hand: ['Just Another Hand', 'cursive']
+			}
+		}
 	},
 	plugins: []
 };
