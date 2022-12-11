85683578d99abb16402a560ec5c96f607d061136
Doug A <douganderson444@gmail.com>
2022-12-11 19:40:24 -0400

    add base, colors, fix flexbox

--- src/lib/Page.svelte
+++ src/lib/Page.svelte
@@ -1,7 +1,7 @@
 <script>
+	import { base } from '$app/paths';
 	import Crossfade from '$lib/Crossfade.svelte';
 	import { data } from '$lib/stores';
-	import { nanoid } from 'nanoid/non-secure';
 	import Expand from '$lib/SVGs/Expand.svelte';
 	import { duration } from '$lib/crossfade';
 	import { fade } from 'svelte/transition';
@@ -18,20 +18,27 @@
 
 <!-- This page div must be absolute so it doesnt jump when the crossfade happens -->
 <!-- Insides are flexbox so it neatly organizes -->
-<div class="absolute flex flex-row flex-wrap ">
+<div
+	class="absolute flex flex-row flex-wrap min-w-[24rem] min-h-[24rem] items-start"
+	style:background-color={pageData?.color}
+>
 	{#if pageData && pageData.children && pageData.children.length}
 		{#each pageData.children as page, p}
 			<Crossfade id={page.id}>
 				<div
-					class="flex-1 flex flex-col flex-wrap w-28 h-28  bg-red-500 m-4 p-2 rounded-lg shadow text-white"
+					style:background-color={$data[page.id]?.color}
+					class="flex-1 flex flex-col flex-wrap w-28 h-28  m-4 p-2 rounded-lg shadow text-white"
 					out:fade={{ duration }}
 				>
-					<a href="/{page.id}" class="select-none cursor-pointer" contenteditable={false}
+					<a href="{base}/{page.id}" class="select-none cursor-pointer" contenteditable={false}
 						><div class="float-right top-0 right-0 h-6 w-6 m-1 text-neutral-800/50">
 							<Expand />
 						</div></a
 					>
-					{page.id}
+					<span class="text-xl text-white/50">{page.id}</span>
+					{$data[page.id].children.length ? $data[page.id].children.length + ' Items' : ''}
+
+					<!-- {$data[page.id]?.color} -->
 				</div>
 			</Crossfade>
 		{/each}
