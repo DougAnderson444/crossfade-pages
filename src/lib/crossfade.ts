9f5ae17f0e39e47d2c3629b752f3270282eba75e
Doug A <douganderson444@gmail.com>
2023-01-03 13:19:07 -0400

    add fade fallback

--- src/lib/crossfade.ts
+++ src/lib/crossfade.ts
@@ -1,8 +1,8 @@
-import { crossfade as svelteCrossfade } from 'svelte/transition';
+import { crossfade as svelteCrossfade, fade } from 'svelte/transition';
 import { sineOut } from 'svelte/easing';
 
 import { writable } from 'svelte/store';
 
 export const duration = 450;
 
-export const crossfade = svelteCrossfade({ duration, easing: sineOut });
+export const crossfade = svelteCrossfade({ duration, easing: sineOut, fallback: fade });
